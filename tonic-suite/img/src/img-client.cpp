8631fa753315f5f4b2454ea6f53d5e8685c9b8ca
Kiran <kiran7193@gmail.com>
2017-11-12 18:59:39 -0800

    Client side RTT measurements. Support for uniform requests per second

--- tonic-suite/img/src/img-client.cpp
+++ tonic-suite/img/src/img-client.cpp
@@ -21,6 +21,7 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <string>
 
 #include "opencv2/opencv.hpp"
 #include "boost/program_options.hpp"
@@ -42,22 +43,9 @@ pthread_t* threads;
 FILE * pFile;
 pthread_rwlock_t output_rwlock = PTHREAD_RWLOCK_INITIALIZER;
 
-/*
-void tic(void) { gettimeofday(&t1, NULL); }
-
-double toc(void) {
-   gettimeofday(&t2, NULL);
-   double elapsedTime;
-   elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
-   elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
- 
-   return elapsedTime;
- }
-*/
 
 unsigned int microseconds = 1000000;
 namespace po = boost::program_options;
-struct timeval tv1, tv2;
 
 unsigned int RPS;
 unsigned int seconds;
@@ -70,7 +58,7 @@ void* create_thread_socket(void *args)
    float* preds = (float*)malloc(app.pl.num * sizeof(float));
 
    for(int i = 0; i < seconds; i++)
-{
+    {
     SOCKET_send(socketfd, (char*)&app.pl.req_name, MAX_REQ_SIZE, 0);
 
     // send len
@@ -93,16 +81,40 @@ for(int i = 0; i < seconds;i ++)
 
    free(preds);
 
+    pthread_exit(NULL);
+
+}
+
+void* create_thread_socket_v1(void *args)
+{
+    int socketfd = CLIENT_init((char*)app.hostname.c_str(), app.portno, 0);
+    float* preds = (float*)malloc(app.pl.num * sizeof(float));
+    struct timeval t_start, t_end;
+    double elapsed_time;
+    int* elapsed_under = (int*)malloc(sizeof(int));
+
+    SOCKET_send(socketfd, (char*)&app.pl.req_name, MAX_REQ_SIZE, 0);
+    SOCKET_txsize(socketfd, app.pl.num * app.pl.size);
+    SOCKET_send(socketfd, (char*)app.pl.data,
+                app.pl.num * app.pl.size * sizeof(float), 0);
+    gettimeofday(&t_start, NULL);
+    SOCKET_receive(socketfd, (char*)preds, app.pl.num * sizeof(float), 0);
+    gettimeofday(&t_end, NULL);
+
+    elapsed_time = (t_end.tv_sec - t_start.tv_sec) * 1000.0;
+    elapsed_time += (t_end.tv_usec - t_start.tv_usec) / 1000.0;
+    //printf("Elapsed time %lf\n", elapsed_time);
+
+    std::string temp = std::to_string(elapsed_time);
     //write to outputfile
-/*
     pthread_rwlock_wrlock(&output_rwlock);
     fwrite(temp.c_str(),sizeof(char),temp.length(), pFile);
     fwrite("\n", sizeof(char), 1, pFile);
     fflush(pFile);
     pthread_rwlock_unlock(&output_rwlock);
-*/
-    pthread_exit(NULL);
 
+    *elapsed_under = (int)elapsed_time;
+    return (void*)elapsed_under;
 }
 
 void reset_djinn()
@@ -170,174 +182,235 @@ po::variables_map parse_opts(int ac, char** av) {
   return vm;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
 
-  po::variables_map vm = parse_opts(argc, argv);
+    po::variables_map vm = parse_opts(argc, argv);
 
-  bool debug = vm["debug"].as<bool>();
+    bool debug = vm["debug"].as<bool>();
 
-  app.task = vm["task"].as<string>();
-  app.network =
-      vm["common"].as<string>() + "configs/" + vm["network"].as<string>();
-  app.weights =
-      vm["common"].as<string>() + "weights/" + vm["weights"].as<string>();
-  app.input = vm["input"].as<string>();
+    app.task = vm["task"].as<string>();
+    app.network = vm["common"].as<string>() + "configs/" + vm["network"].as<string>();
+    app.weights = vm["common"].as<string>() + "weights/" + vm["weights"].as<string>();
+    app.input = vm["input"].as<string>();
 
-  RPS = vm["rps"].as<int>();
-  seconds = vm["seconds"].as<int>();
-  total_requests = RPS * seconds;
-  sleep_time = (1000000.0f / double(RPS));
+    RPS = vm["rps"].as<int>();
+    seconds = vm["seconds"].as<int>();
+    total_requests = RPS * seconds;
+    sleep_time = (1000000.0f / double(RPS));
 
   
-  threads = (pthread_t*)malloc(sizeof(pthread_t)*RPS);
-  // DjiNN service or local?
-  app.djinn = vm["djinn"].as<bool>();
-  app.gpu = vm["gpu"].as<bool>();
+    threads = (pthread_t*)malloc(sizeof(pthread_t)*RPS*seconds);
+    // DjiNN service or local?
+    app.djinn = vm["djinn"].as<bool>();
+    app.gpu = vm["gpu"].as<bool>();
 
-  if (app.djinn) {
-    app.hostname = vm["hostname"].as<string>();
-    app.portno = vm["portno"].as<int>();
-    //ddapp.socketfd = CLIENT_init((char*)app.hostname.c_str(), app.portno, debug);
-    //if (app.socketfd < 0) exit(0);
-  } else {
-    app.net = new Net<float>(app.network);
-    app.net->CopyTrainedLayersFrom(app.weights);
-    Caffe::set_phase(Caffe::TEST);
-    if (app.gpu)
-      Caffe::set_mode(Caffe::GPU);
-    else
-      Caffe::set_mode(Caffe::CPU);
-  }
-
-  // send req_type
-  app.pl.size = 0;
-  // hardcoded for AlexNet
-  strcpy(app.pl.req_name, app.task.c_str());
-  if (app.task == "imc") app.pl.size = 3 * 227 * 227;
-  // hardcoded for DeepFace
-  else if (app.task == "face")
-    app.pl.size = 3 * 152 * 152;
-  // hardcoded for Mnist
-  else if (app.task == "dig")
-    app.pl.size = 1 * 28 * 28;
-  else
-    LOG(FATAL) << "Unrecognized task.\n";
-
-  // read in images
-  // cmt: using map, cant use duplicate names for images
-  // change to other structure (vector) if you want to send the same exact
-  // filename multiple times
-  vector<pair<string, Mat> > imgs;
-  std::ifstream file(app.input.c_str());
-  std::string img_file;
-  app.pl.num = 0;
-  while (getline(file, img_file)) {
-    LOG(INFO) << "Reading " << img_file;
-    Mat img;
-    if (app.task == "dig")
-      img = imread(img_file, CV_LOAD_IMAGE_GRAYSCALE);
-    else
-      img = imread(img_file);
-
-    if (img.channels() * img.rows * img.cols != app.pl.size)
-      LOG(ERROR) << "Skipping " << img_file
-                 << ", resize to correct dimensions.\n";
-    else {
-      imgs.push_back(make_pair(img_file, img));
-      ++app.pl.num;
+    if (app.djinn)
+    {
+        app.hostname = vm["hostname"].as<string>();
+        app.portno = vm["portno"].as<int>();
+        //ddapp.socketfd = CLIENT_init((char*)app.hostname.c_str(), app.portno, debug);
+        //if (app.socketfd < 0) exit(0);
     }
-  }
-
-  if (app.pl.num < 1) LOG(FATAL) << "No images read!";
-
-  vector<pair<string, Mat> >::iterator it;
-  // align facial recognition image
-  if (app.task == "face" && vm["align"].as<bool>()) {
-    for (it = imgs.begin(); it != imgs.end(); ++it) {
-      LOG(INFO) << "Aligning: " << it->first << endl;
-      preprocess(it->second, vm["flandmark"].as<string>(),
-                 vm["haar"].as<string>());
-      // comment in save + view aligned image
-      // imwrite(it->first+"_a", it->second);
+    else
+    {
+        app.net = new Net<float>(app.network);
+        app.net->CopyTrainedLayersFrom(app.weights);
+        Caffe::set_phase(Caffe::TEST);
+        if (app.gpu)
+            Caffe::set_mode(Caffe::GPU);
+        else
+            Caffe::set_mode(Caffe::CPU);
     }
-  }
 
-  // prepare data into array
-  app.pl.data = (float*)malloc(app.pl.num * app.pl.size * sizeof(float));
-  float* preds = (float*)malloc(app.pl.num * sizeof(float));
+    // send req_type
+    app.pl.size = 0;
+    // hardcoded for AlexNet
+    strcpy(app.pl.req_name, app.task.c_str());
+    if (app.task == "imc")
+        app.pl.size = 3 * 227 * 227;
 
-  int img_count = 0;
-  for (it = imgs.begin(); it != imgs.end(); ++it) {
-    int pix_count = 0;
-    for (int c = 0; c < it->second.channels(); ++c) {
-      for (int i = 0; i < it->second.rows; ++i) {
-        for (int j = 0; j < it->second.cols; ++j) {
-          Vec3b pix = it->second.at<Vec3b>(i, j);
-          float* p = (float*)(app.pl.data);
-          p[img_count * app.pl.size + pix_count] = pix[c];
-          ++pix_count;
+    // hardcoded for DeepFace
+    else if (app.task == "face")
+        app.pl.size = 3 * 152 * 152;
+
+    // hardcoded for Mnist
+    else if (app.task == "dig")
+        app.pl.size = 1 * 28 * 28;
+    else
+        LOG(FATAL) << "Unrecognized task.\n";
+
+    // read in images
+    // cmt: using map, cant use duplicate names for images
+    // change to other structure (vector) if you want to send the same exact
+    // filename multiple times
+    vector<pair<string, Mat> > imgs;
+    std::ifstream file(app.input.c_str());
+    std::string img_file;
+    app.pl.num = 0;
+    while (getline(file, img_file))
+    {
+        LOG(INFO) << "Reading " << img_file;
+        Mat img;
+        if (app.task == "dig")
+            img = imread(img_file, CV_LOAD_IMAGE_GRAYSCALE);
+        else
+            img = imread(img_file);
+
+        if (img.channels() * img.rows * img.cols != app.pl.size)
+            LOG(ERROR) << "Skipping " << img_file << ", resize to correct dimensions.\n";
+        else
+        {
+            imgs.push_back(make_pair(img_file, img));
+            ++app.pl.num;
         }
-      }
     }
-    ++img_count;
-  }
-/*
-	//Create log file
-   pFile = fopen("out.txt","w");
-   if(pFile == NULL){
-         LOG(INFO) << "Could not create out file";
-         return 0;
-         }
-*/
-  if (app.djinn) {
 
-  for(int i = 0; i < RPS; i++)
-  {
-	pthread_create(threads+i,NULL,create_thread_socket,NULL);
-  }
+    if (app.pl.num < 1)
+        LOG(FATAL) << "No images read!";
 
-   for (int i=0; i < RPS; i++)
-        pthread_join(threads[i], NULL);
+    vector<pair<string, Mat> >::iterator it;
+    // align facial recognition image
+    if (app.task == "face" && vm["align"].as<bool>())
+    {
+        for (it = imgs.begin(); it != imgs.end(); ++it)
+        {
+            LOG(INFO) << "Aligning: " << it->first << endl;
+            preprocess(it->second, vm["flandmark"].as<string>(), vm["haar"].as<string>());
+            // comment in save + view aligned image
+            // imwrite(it->first+"_a", it->second);
+        }
+    }
 
-   reset_djinn();
-/*
-    for (int i=0dd; i<seconds; i++) 
-    SOCKET_send(app.socketfd, (char*)&app.pl.req_name, MAX_REQ_SIZE, debug);
+    // prepare data into array
+    app.pl.data = (float*)malloc(app.pl.num * app.pl.size * sizeof(float));
+    float* preds = (float*)malloc(app.pl.num * sizeof(float));
 
-    // send len
-    SOCKET_txsize(app.socketfd, app.pl.num * app.pl.size);
+    int img_count = 0;
+ 
+    for (it = imgs.begin(); it != imgs.end(); ++it)
+    {
+        int pix_count = 0;
+        for (int c = 0; c < it->second.channels(); ++c)
+        {
+            for (int i = 0; i < it->second.rows; ++i)
+            {
+                for (int j = 0; j < it->second.cols; ++j)
+                {
+                    Vec3b pix = it->second.at<Vec3b>(i, j);
+                    float* p = (float*)(app.pl.data);
+                    p[img_count * app.pl.size + pix_count] = pix[c];
+                    ++pix_count;
+                }
+            }
+        }
+        ++img_count;
+    }
 
-    // send image(s)
-    SOCKET_send(app.socketfd, (char*)app.pl.data,
-                app.pl.num * app.pl.size * sizeof(float), debug);
+    //Create log file
+    std::string outfileName;
+    outfileName = std::to_string(RPS) + ".out";
+    pFile = fopen(outfileName.c_str(),"w");
+    if(pFile == NULL){
+	    LOG(INFO) << "Could not create out file";
+	    return 0;
+	}
+    unsigned int sleep_time = 1000000 / RPS;
+    if (app.djinn)
+    {
+        if (seconds > 1)
+        {
+            for(int i = 0; i < seconds*RPS; i++)
+            {
+                pthread_create(threads+i,NULL,create_thread_socket_v1,NULL);
+                usleep(sleep_time);
+            }
 
-    SOCKET_receive(app.socketfd, (char*)preds, app.pl.num * sizeof(float),
-                   debug);
+            for (int i=0; i < seconds*RPS; i++)
+                pthread_join(threads[i], NULL);
+        }
 
-    usleep(microseconds);		
-    
-    SOCKET_close(app.socketfd, debug);
+        else
+        {
+            int* elapsed = (int*)calloc(sizeof(int), 14);
+            for(int i = 0; i < RPS; i++)
+                pthread_create(threads+i,NULL,create_thread_socket_v1,NULL);
+            for(int i=0; i < RPS; i++)
+            {
+                void* tmp = NULL;
+                pthread_join(threads[i], &tmp);
+                int tmp_int = *(int*) tmp;
+                if (tmp_int == 0)
+                    elapsed[0]++;
+                else if (tmp_int == 1)
+                    elapsed[1]++;
+                else if (tmp_int == 2)
+                    elapsed[2]++;
+                else if (tmp_int == 3)
+                    elapsed[3]++;
+                else if (tmp_int == 4)
+                    elapsed[4]++;
+                else if (tmp_int == 5)
+                    elapsed[5]++;
+                else if (tmp_int == 6)
+                    elapsed[6]++;
+                else if (tmp_int == 7)
+                    elapsed[7]++;
+                else if (tmp_int == 8)
+                    elapsed[8]++;
+                else if (tmp_int == 9)
+                    elapsed[9]++;
+                else if ((tmp_int >= 10)&&(tmp_int < 20))
+                    elapsed[10]++;
+                else if ((tmp_int >= 20)&&(tmp_int < 40))
+                    elapsed[11]++;
+                else if ((tmp_int >= 40)&&(tmp_int < 60))
+                    elapsed[12]++;
+                else
+                    elapsed[13]++;
+            }
+            FILE *fresp;
+            fresp = fopen("responses.txt", "a");
+            std::string rps_string = std::to_string(RPS);
+            fwrite("\n", sizeof(char), 1, fresp);
+            fwrite("\n", sizeof(char), 1, fresp);
+            fwrite(rps_string.c_str(), sizeof(char), rps_string.length(), fresp);
+            fwrite("\n", sizeof(char), 1, fresp);
+            fwrite("\n", sizeof(char), 1, fresp);
+            for(int i=0; i < 14; i++)
+            {
+                std::string time = std::to_string(elapsed[i]);
+                fwrite(time.c_str(), sizeof(char), time.length(), fresp);
+                fwrite("\n", sizeof(char), 1, fresp);
+            }
+            fwrite("----", sizeof(char)*4, 1, fresp);
+            fflush(fresp);
 
-*/
-  } else {
-    float loss;
-    reshape(app.net, app.pl.num * app.pl.size);
+        }
 
-    vector<Blob<float>*> in_blobs = app.net->input_blobs();
-    in_blobs[0]->set_cpu_data((float*)app.pl.data);
-    vector<Blob<float>*> out_blobs = app.net->ForwardPrefilled(&loss);
-    memcpy(preds, out_blobs[0]->cpu_data(), app.pl.num * sizeof(float));
-  }
+        reset_djinn();
+    }
 
-  for (it = imgs.begin(); it != imgs.end(); it++) {
-    LOG(INFO) << "Image: " << it->first
-              << " class: " << preds[distance(imgs.begin(), it)] << endl;
-    ;
-  }
+    else
+    {
+        float loss;
+        reshape(app.net, app.pl.num * app.pl.size);
 
-  if (!app.djinn) free(app.net);
-  free(app.pl.data);
-  free(preds);
-  free(threads);
-  return 0;
+        vector<Blob<float>*> in_blobs = app.net->input_blobs();
+        in_blobs[0]->set_cpu_data((float*)app.pl.data);
+        vector<Blob<float>*> out_blobs = app.net->ForwardPrefilled(&loss);
+        memcpy(preds, out_blobs[0]->cpu_data(), app.pl.num * sizeof(float));
+    }
+
+    for (it = imgs.begin(); it != imgs.end(); it++)
+        LOG(INFO) << "Image: " << it->first << " class: " << preds[distance(imgs.begin(), it)] << endl;
+
+    if (!app.djinn)
+        free(app.net);
+
+    free(app.pl.data);
+    free(preds);
+    free(threads);
+    return 0;
+
 }
